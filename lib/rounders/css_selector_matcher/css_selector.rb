9a8a634fcc8266ceade055efa0ce02439b3b463a
akira.takahashi <rike422@gmail.com>
2016-03-23 00:50:20 +0900

    Implement css_selecotr_matcher

--- lib/rounders/css_selector_matcher/css_selector.rb
+++ lib/rounders/css_selector_matcher/css_selector.rb
@@ -1,10 +1,17 @@
 module Rounders
   module CssSelectorMatcher
-    class CssSelector < Rounders::Machers::Matcher
+    class CssSelector < Rounders::Matchers::Matcher
+      attr_reader :pattern
+
+      def initialize(pattern)
+        @pattern = pattern
+      end
+
       def match(mail)
         return if mail.html_part.blank?
-        html_part = Nokogiri::HTML(mail.html_part)
-        html_part.css(pattern)
+        html_part = Nokogiri::HTML(mail.html_part.body.to_s)
+        node = html_part.css(pattern)
+        node.present? ? node : nil
       end
     end
   end
