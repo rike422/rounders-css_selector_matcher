1892692dad455896921f6b756230b7fce9103dd0
akira.takahashi <rike422@gmail.com>
2016-03-23 21:17:12 +0900

    Add spec

--- spec/rounders/css_selector_matcher/css_selector_spec.rb
+++ spec/rounders/css_selector_matcher/css_selector_spec.rb
@@ -5,23 +5,40 @@ describe Rounders::CssSelectorMatcher::CssSelector do
   let(:described_instance) { described_class.new(*arguments) }
   let(:selector) { 'body div.header h2' }
   let(:arguments) { [selector] }
-  let(:mail) do
-    mail = Mail.new do
-      html_part do
-        content_type 'text/html; charset=UTF-8'
-        body <<EOF
+  let(:html_body) do
+    <<-EOF
     <div class="header">
       <h2> head text </h2>
     </div>
     <div class="footer">
       <h2> foot text </h2>
     </div>
-EOF
+    EOF
+  end
+  let(:mail) do
+    body = html_body
+    mail = Mail.new do
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body body
       end
     end
   end
   describe '#match' do
     subject { described_instance.match(mail) }
     it { expect(subject.to_s).to eql '<h2> head text </h2>' }
+    context 'if not matched' do
+      let(:html_body) do
+        <<-EOF
+    <div class="dummy-header">
+      <h2> head text </h2>
+    </div>
+    <div class="footer">
+      <h2> foot text </h2>
+    </div>
+        EOF
+      end
+      it { expect(subject).to be_nil }
+    end
   end
 end
